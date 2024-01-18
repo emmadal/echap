cc52e6dfa94d2f2e2033689545760956c32696be
emmadal <emmanueldalougou@gmail.com>
2024-01-18 03:48:08 +0000

    configure relative import

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          'types': './src/types/',
+          'screens': './src/screens/',
+          'components': './src/components/',
+          'constants': './src/constants/',
+          'navigation': './src/routes/',
+          'utils': './src/utils/',
+          'hooks': './src/hooks/',
+          'api': './src/api/',
+        },
+      },
+    ],
+  ],
 };
